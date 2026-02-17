6acbf030b3f2a078b2be01a1c5c7e874deabef2d
Muhammet Esat Demir <muhammetesatdemir@outlook.com>
2026-02-17 14:58:49 +0300

    Add project files.

---
+++ FirstMicroService.Todos/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FirstMicroService.Todos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstMicroService.Todos.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
---
+++ FirstMicroService.Todos/Models/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstMicroService.Todos.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Work { get; set; } = string.Empty;
+    }
+}
